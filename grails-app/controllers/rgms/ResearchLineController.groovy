c16581112e0da2b1ae0cdc8dfa1f789f45efd441
eggn <eggn@cin.ufpe.br>
2012-04-02 19:21:10 -0300

    Feature: Lisnhas de pesquisa

--- grails-app/controllers/rgms/ResearchLineController.groovy
+++ grails-app/controllers/rgms/ResearchLineController.groovy
@@ -26,14 +26,14 @@ class ResearchLineController {
             return
         }
 
-		flash.message = message(code: 'default.created.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), researchLineInstance.id])
+        flash.message = message(code: 'default.created.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), researchLineInstance.id])
         redirect(action: "show", id: researchLineInstance.id)
     }
 
     def show() {
         def researchLineInstance = ResearchLine.get(params.id)
         if (!researchLineInstance) {
-			flash.message = message(code: 'default.not.found.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), params.id])
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), params.id])
             redirect(action: "list")
             return
         }
@@ -64,39 +64,60 @@ class ResearchLineController {
             def version = params.version.toLong()
             if (researchLineInstance.version > version) {
                 researchLineInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                          [message(code: 'researchLine.label', default: 'ResearchLine')] as Object[],
+                    [message(code: 'researchLine.label', default: 'ResearchLine')] as Object[],
                           "Another user has updated this ResearchLine while you were editing")
                 render(view: "edit", model: [researchLineInstance: researchLineInstance])
                 return
             }
         }
-
+        
+        def tempPublications = new ArrayList(researchLineInstance.publications);
         researchLineInstance.properties = params
-
+        
         if (!researchLineInstance.save(flush: true)) {
+            
             render(view: "edit", model: [researchLineInstance: researchLineInstance])
             return
         }
+        for(p in tempPublications)
+        {                
+            if(!researchLineInstance?.publications.contains(p))
+            {
+                //def pub = Publication.get(p.id);
+                p.researchLine = null;                
+                if(!p.save(flush:true))
+                {
+                    render(view: "edit", model: [researchLineInstance: researchLineInstance])
+                    return
+                }
+            }
+        }
+        
 
-		flash.message = message(code: 'default.updated.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), researchLineInstance.id])
+        flash.message = message(code: 'default.updated.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), researchLineInstance.id])
         redirect(action: "show", id: researchLineInstance.id)
     }
 
     def delete() {
-        def researchLineInstance = ResearchLine.get(params.id)
-        if (!researchLineInstance) {
-			flash.message = message(code: 'default.not.found.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), params.id])
-            redirect(action: "list")
-            return
-        }
+        def researchLineInstance = ResearchLine.get(params.id)        
 
         try {
+            for(p in researchLineInstance?.publications)
+            {           
+               // def pub = Publication.get(p.id);
+                p.researchLine = null;                
+                if(!p.save(flush:true))
+                {
+                    render(view: "edit", model: [researchLineInstance: researchLineInstance])
+                    return
+                }                
+            }       
             researchLineInstance.delete(flush: true)
-			flash.message = message(code: 'default.deleted.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), params.id])
+            flash.message = message(code: 'default.deleted.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), params.id])
             redirect(action: "list")
         }
         catch (DataIntegrityViolationException e) {
-			flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), params.id])
+            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), params.id])
             redirect(action: "show", id: params.id)
         }
     }
--- grails-app/domain/rgms/ResearchLine.groovy
+++ grails-app/domain/rgms/ResearchLine.groovy
@@ -5,17 +5,13 @@ class ResearchLine {
     String name
     String description           
     static hasMany = [publications : Publication]
-    static mapping = {
-      publications cascade:"save-update"
-  }   
     static constraints = {
         name(blank:false, unique:true)
-        description(maxSize:256,blank:false)
+        description(maxSize:256,blank:false)        
     }
         
     String toString()
     {
         return name;
     }
-    
 }
