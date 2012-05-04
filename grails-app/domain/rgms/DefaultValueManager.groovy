423b7bb0e346acb1d870695e3b7564c7867cde7b
eggn <eggn@cin.ufpe.br>
2012-05-04 09:25:00 -0300

    FEATURE: Valores default versao 1

---
+++ grails-app/domain/rgms/DefaultValueManager.groovy
@@ -0,0 +1,41 @@
+package rgms
+
+public class DefaultValueManager {
+	
+	private static final filePath = "..//..//conf//DefaultValues.properties"
+	
+	public static final Univeristy = "University"
+	public static final City = "City"
+	public static final Country = "Country"
+	public static final Domain = "Domain"
+	
+	private Properties properties = null;
+
+	private static DefaultValueManager instance = null;
+	
+	public DefaultValueManager(){
+		properties = new Properties()
+		properties.load(new FileInputStream(filePath))
+//		properties.keySet()
+
+		println(properties.get("Domain"))
+		println(properties)
+	}
+	
+	public static DefaultValueManager getInstance(){
+		println()
+		println("Instance=*"+instance)
+		println(instance?.getProperty("University"))
+		if(!instance)
+			instance = new DefaultValueManager()
+		
+		return instance
+	}
+	
+	public String getPropertyValue(name){
+		return properties.get(name)
+	}
+	
+	
+
+}
