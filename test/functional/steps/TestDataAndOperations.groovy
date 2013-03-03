90bcb3ac5d263b94980e7deb2c4c586718bcdea8
Marcelo Alves <mma2@cin.ufpe.br>
2013-03-03 20:15:47 -0300

    Criacao de Testes ResearchGroup

--- test/functional/steps/TestDataAndOperations.groovy
+++ test/functional/steps/TestDataAndOperations.groovy
@@ -1,7 +1,14 @@
 package steps
 
+import java.text.SimpleDateFormat
+
+import rgms.member.Member
+import rgms.member.MembershipController
+import rgms.member.ResearchGroup
+import rgms.member.ResearchGroupController
 import rgms.publication.Periodico
 import rgms.publication.PeriodicoController
+import rgms.publication.PublicationController
 
 class TestDataAndOperations {
     static articles = [
@@ -43,6 +50,36 @@ class TestDataAndOperations {
         cont.save()
         cont.response.reset()
     }
+	
+	static public void createResearchGroup(String name, description) {
+		def researchGroupController = new ResearchGroupController()
+		researchGroupController.params << [name: name] << [description: description]
+		researchGroupController.request.setContent(new byte[1000]) // Could also vary the request content.
+		researchGroupController.create()
+		researchGroupController.save()
+		researchGroupController.response.reset()
+	}
+
+	static public void createResearchGroupMembershipWithMember(String name, String memberUsername) {
+		def researchGroup = ResearchGroup.findByName(name) 
+		def member = Member.findByUsername(memberUsername)
+		def memberShipController = new MembershipController()
+		memberShipController.params << [researchGroup: researchGroup] << [member: member]
+		memberShipController.create()
+		memberShipController.save()
+		memberShipController.response.reset()
+	}
+
+	static public void createMemberPublication(String username, String title, String date) {
+		def member = Member.findByUsername(username)
+		def publicationController =	 new PublicationController()
+		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");  
+		java.sql.Date dateFormat = new java.sql.Date(format.parse(date).getTime());
+		publicationController.params << [title: title] << [publicationDate: date]
+		publicationController.create()
+		publicationController.save()
+		publicationController.response.reset()
+	}
 
     static void clearArticles() {
         Periodico.findAll()*.delete flush: true // Could also delete the created files.
