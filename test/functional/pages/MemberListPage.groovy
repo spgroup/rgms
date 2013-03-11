277779e1ba92798de756e55ad3396f4aebc9f5e0
Hugo Neiva <hugoneivamelo@gmail.com>
2013-03-10 22:29:20 -0300

    [Equipe 7] Casos de teste e correção de bug.
    Implementação dos casos de teste relativos a feature Reports e correção
    da issue #85.

---
+++ test/functional/pages/MemberListPage.groovy
@@ -0,0 +1,18 @@
+package pages
+
+import geb.Page
+
+class MemberListPage extends Page {
+    static url = "member/list"
+
+    static at = {
+        title ==~ /Member Listagem/
+    }
+
+    static content = {
+    }
+
+    def selectMember(String s) {
+        $('div').find('a', text: s).click()
+    }
+}
