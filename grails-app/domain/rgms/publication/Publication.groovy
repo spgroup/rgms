35596d7201f1e9cccb5ad34df98013e5faa77f0b
Rodrigo Andrade <rcaa2@cin.ufpe.br>
2013-03-27 15:33:38 -0300

    Update Publication.groovy

--- grails-app/domain/rgms/publication/Publication.groovy
+++ grails-app/domain/rgms/publication/Publication.groovy
@@ -15,7 +15,7 @@ abstract class Publication {
     static constraints = {
         title nullable: false, blank: false
         publicationDate nullable: false
-        file maxSize: 100000
+        file maxSize: 100000, nullable: true, blank: true
         researchLine nullable: true, blank: true
     }
 
