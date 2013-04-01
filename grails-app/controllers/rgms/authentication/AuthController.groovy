84d78bf8b4b4ea03251403861879d5191c3bd11e
ros3cin <ros3@cin.ufpe.br>
2013-04-01 00:13:33 -0300

    Import duplicado - AuthController

--- grails-app/controllers/rgms/authentication/AuthController.groovy
+++ grails-app/controllers/rgms/authentication/AuthController.groovy
@@ -11,7 +11,7 @@ import org.apache.shiro.crypto.hash.Sha256Hash
 import rgms.member.Member;
 import rgms.member.PasswordResetRequest;
 
-import java.security.SecureRandom
+
 
 class AuthController {
     def shiroSecurityManager
