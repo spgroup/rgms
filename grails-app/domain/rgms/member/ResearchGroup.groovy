2ef635159862b949c93740bdd05e8ced1732ff8d
Marcelo Alves <mma2@cin.ufpe.br>
2013-04-01 01:25:40 -0300

    Identificação de problemas de código detectáveis via análise estática

--- grails-app/domain/rgms/member/ResearchGroup.groovy
+++ grails-app/domain/rgms/member/ResearchGroup.groovy
@@ -27,4 +27,25 @@ class ResearchGroup {
 		def memberships = Membership.getAllMembers(researchGroup)
 		return Publication.getPublicationsByMembershipList(memberships)
 	}
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ResearchGroup other = (ResearchGroup) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
