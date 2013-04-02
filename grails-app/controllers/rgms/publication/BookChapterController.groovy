a1c858691ef03f23050b127a60479fee32982731
Welington <ettoalmeida@gmail.com>
2013-04-01 23:50:42 -0300

    Modularidade e reuso de codigo

--- grails-app/controllers/rgms/publication/BookChapterController.groovy
+++ grails-app/controllers/rgms/publication/BookChapterController.groovy
@@ -1,11 +1,10 @@
 package rgms.publication
 
-import org.springframework.dao.DataIntegrityViolationException
-
 
 class BookChapterController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	AuxiliarController aux = new AuxiliarController();
 
     def index() {
         redirect(action: "list", params: params)
@@ -27,78 +26,52 @@ class BookChapterController {
             render(view: "create", model: [bookChapterInstance: bookChapterInstance])
             return
         }
-
         flash.message = message(code: 'default.created.message', args: [message(code: 'bookChapter.label', default: 'BookChapter'), bookChapterInstance.id])
         redirect(action: "show", id: bookChapterInstance.id)
     }
 
     def show(Long id) {
         def bookChapterInstance = BookChapter.get(id)
-        if (!bookChapterInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'bookChapter.label', default: 'BookChapter'), id])
-            redirect(action: "list")
-            return
-        }
-
-        [bookChapterInstance: bookChapterInstance]
+		boolean isReturned = aux.check(id, bookChapterInstance, 'bookChapter.label', 'BookChapter');
+		if(!isReturned){
+			[bookChapterInstance: bookChapterInstance]			
+		}
     }
 
     def edit(Long id) {
         def bookChapterInstance = BookChapter.get(id)
-        if (!bookChapterInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'bookChapter.label', default: 'BookChapter'), id])
-            redirect(action: "list")
-            return
-        }
-
-        [bookChapterInstance: bookChapterInstance]
+		boolean isReturned = aux.check(id, bookChapterInstance, 'bookChapter.label', 'BookChapter');
+		if(!isReturned){
+			[bookChapterInstance: bookChapterInstance]
+		}
     }
 
+	def delete(Long id) {
+		def bookChapterInstance = BookChapter.get(id)
+		aux.delete(id, bookChapterInstance, 'bookChapter.label', 'BookChapter');
+	}
+	
     def update(Long id, Long version) {
         def bookChapterInstance = BookChapter.get(id)
-        if (!bookChapterInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'bookChapter.label', default: 'BookChapter'), id])
-            redirect(action: "list")
-            return
-        }
-
-        if (version != null) {
-            if (bookChapterInstance.version > version) {
-                bookChapterInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                          [message(code: 'bookChapter.label', default: 'BookChapter')] as Object[],
-                          "Another user has updated this BookChapter while you were editing")
-                render(view: "edit", model: [bookChapterInstance: bookChapterInstance])
-                return
-            }
-        }
-
-        bookChapterInstance.properties = params
-
-        if (!bookChapterInstance.save(flush: true)) {
-            render(view: "edit", model: [bookChapterInstance: bookChapterInstance])
-            return
-        }
-
-        flash.message = message(code: 'default.updated.message', args: [message(code: 'bookChapter.label', default: 'BookChapter'), bookChapterInstance.id])
-        redirect(action: "show", id: bookChapterInstance.id)
+        boolean isReturned = aux.check(id, bookChapterInstance, 'bookChapter.label', 'BookChapter');
+		if(!isReturned){
+			if (version != null) {
+				if (bookChapterInstance.version > version) {
+					bookChapterInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+							[message(code: 'bookChapter.label', default: 'BookChapter')] as Object[],
+							"Another user has updated this BookChapter while you were editing")
+					render(view: "edit", model: [bookChapterInstance: bookChapterInstance])
+					return
+				}
+			}
+			bookChapterInstance.properties = params
+			if (!bookChapterInstance.save(flush: true)) {
+				render(view: "edit", model: [bookChapterInstance: bookChapterInstance])
+				return
+			}
+			flash.message = message(code: 'default.updated.message', args: [message(code: 'bookChapter.label', default: 'BookChapter'), bookChapterInstance.id])
+			redirect(action: "show", id: bookChapterInstance.id)
+		}
     }
-
-    def delete(Long id) {
-        def bookChapterInstance = BookChapter.get(id)
-        if (!bookChapterInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'bookChapter.label', default: 'BookChapter'), id])
-            redirect(action: "list")
-            return
-        }
-
-        try {
-            bookChapterInstance.delete(flush: true)
-            flash.message = message(code: 'default.deleted.message', args: [message(code: 'bookChapter.label', default: 'BookChapter'), id])
-            redirect(action: "list")
-        }
-        catch (DataIntegrityViolationException e) {
-            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'bookChapter.label', default: 'BookChapter'), id])
-            redirect(action: "show", id: id)
-        }
-    }
+	
 }
