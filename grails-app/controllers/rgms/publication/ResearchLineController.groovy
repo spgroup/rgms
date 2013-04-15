7d082f054c915e5a14e01983a4ce20ad33e8f403
rsmbf <rsmbf@cin.ufpe.br>
2013-04-14 21:23:01 -0300

    Feature notificacao researchLine

--- grails-app/controllers/rgms/publication/ResearchLineController.groovy
+++ grails-app/controllers/rgms/publication/ResearchLineController.groovy
@@ -1,119 +1,198 @@
 package rgms.publication
 
+
 import org.springframework.dao.DataIntegrityViolationException
+import rgms.member.Member
 
 class ResearchLineController {
 
-    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
-    def index() {
-        redirect(action: "list", params: params)
-    }
+	def index() {
+		redirect(action: "list", params: params)
+	}
 
-    def list() {
-        params.max = Math.min(params.max ? params.int('max') : 10, 100)
-        [researchLineInstanceList: ResearchLine.list(params), researchLineInstanceTotal: ResearchLine.count()]
-    }
+	def list() {
+		params.max = Math.min(params.max ? params.int('max') : 10, 100)
+		[researchLineInstanceList: ResearchLine.list(params), researchLineInstanceTotal: ResearchLine.count()]
+	}
 
-    def create() {
-        [researchLineInstance: new ResearchLine(params)]
-    }
+	def create() {
+		[researchLineInstance: new ResearchLine(params)]
+	}
 
-    def save() {
-        def researchLineInstance = new ResearchLine(params)
-        saveResearchLine("create", researchLineInstance, "created")
-    }
+	def save() {		
+		def researchLineInstance = new ResearchLine(params)
+		saveResearchLine("create", researchLineInstance, "created",[])
+	}
 
-    def show() {
-        tryFindInstance(params)
-    }
+	def show() {
+		tryFindInstance(params)
+	}
 
-    def edit() {
-        tryFindInstance(params)
-    }
+	def edit() {
+		tryFindInstance(params)
+	}
+	
+	def getOldMembers(def id)
+	{
+		def oldMembers = ResearchLine.get(id)?.members
+		def members = []
+		for(m in oldMembers)
+		{
+			members.add(Member.get(m.id))
+		}
+		members
+	}
 
-    def update() {
-        def researchLineInstance = getInstance(params)
-        if(!researchLineInstance)
-           return 
+	def update() {
+		def old_members = getOldMembers(params.id)
+		def researchLineInstance = getInstance(params)		
+		if(!researchLineInstance)
+			return
 
 		if (!checkInstanceVersion(params, researchLineInstance))
 			return
-        researchLineInstance.properties = params
-        saveResearchLine("edit", researchLineInstance, "updated")
-    }
+		researchLineInstance.properties = params
+		if(!params.members)
+		{
+			researchLineInstance.properties.members = []
+		}
+		saveResearchLine("edit", researchLineInstance, "updated",old_members)
+	}
+	
+	def sendEmail(def email, def title, def content)
+	{
+		sendMail {
+			to email
+			from grailsApplication.config.grails.mail.username
+			subject title
+			body content
+			//#end
+		}
+	}
+	
+	def sendNotifications(def researchLineInstance,def oldMembers)
+	{
+		def receivedMembers = researchLineInstance?.members
+		def similarMembers = receivedMembers.intersect(oldMembers)
+		def newMembers = receivedMembers.findAll {member -> !similarMembers.contains(member)}
+		def membersWhoLeft = oldMembers.findAll {member -> !similarMembers.contains(member)}
+		def researchLineName = researchLineInstance.name
+		def contentJoined = "The following members joined the research line "+researchLineName+":\n"
+		def contentLeft = "The following member left the research line "+researchLineName+":\n"
+		def title = "You are not a member of the Research Line "+ researchLineName + " anymore"
+		for(m in membersWhoLeft)
+		{
+			def content = "Hello ${ m.name},\n\nYou are not participating of the research line "+researchLineName+" anymore.\n\nBest Regards,\n Admin".toString()
+			sendEmail(m.email,title,content)
+			contentLeft += "* "+m.name + "\n"
+		}
+		
+		title = "You are now a member of the Research Line "+ researchLineName
+		for(m in newMembers)
+		{
+			def content = "Hello ${ m.name},\n\nYou are now participating of the research line "+researchLineName+".\n\nBest Regards,\n Admin".toString()
+			sendEmail(m.email,title,content)
+			contentJoined += "* "+m.name + "\n "
+		}
+		title = "Report of change of members in Research Line "+researchLineName 
+		
+		def membersJoined = newMembers.size() > 0
+		def membersLeft =  membersWhoLeft.size() > 0
 
-    def delete() {
-        def researchLineInstance = ResearchLine.get(params.id)        
-        try {
-            if (!editPublications(researchLineInstance))
-               return
-            researchLineInstance.delete(flush: true)
-            createMessage(action: "list")
-        }
-        catch (DataIntegrityViolationException) {
-            createMessage(action: "show", id: params.id)
-        }
-    }
+		for(m in similarMembers)
+		{
+			def content = "Hello ${ m.name},\n\n"
+			
+			if(membersJoined)
+			{
+				content+= contentJoined+"\n"
+			}
+			if(membersLeft)
+			{
+				content+= contentLeft+"\n"
+			}
+			if(membersJoined || membersLeft)
+			{
+				content += "\nBest Regards,\n Admin"
+				sendEmail(m.email,title,content)
+			}
+		}
+	}
 
-    def checkInstanceVersion(params, researchLineInstance){
-        if (params.version) {
-            def version = params.version.toLong()
-            if (researchLineInstance.version > version) {
-                researchLineInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                        [message(code: 'researchLine.label', default: 'ResearchLine')] as Object[],
-                        "Another user has updated this ResearchLine while you were editing")
-                render(view: "edit", model: [researchLineInstance: researchLineInstance])
-                return false
-            }
-        }
-        return true
-    }
+	def delete() {
+		def researchLineInstance = ResearchLine.get(params.id)
+		try {
+			if (!editPublications(researchLineInstance))
+				return
+			researchLineInstance.delete(flush: true)
+			createMessage(action: "list")
+		}
+		catch (DataIntegrityViolationException) {
+			createMessage(action: "show", id: params.id)
+		}
+	}
 
-    def editPublications(researchLineInstance){
-        for(p in researchLineInstance?.publications)
-        {
-            p.researchLine = null;
-            if(!p.save(flush:true))
-            {
-                render(view: "edit", model: [researchLineInstance: researchLineInstance])
-                return false
-            }
-        }
-        true
-    }
+	def checkInstanceVersion(params, researchLineInstance){
+		if (params.version) {
+			def version = params.version.toLong()
+			if (researchLineInstance.version > version) {
+				researchLineInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+						[message(code: 'researchLine.label', default: 'ResearchLine')] as Object[],
+						"Another user has updated this ResearchLine while you were editing")
+				render(view: "edit", model: [researchLineInstance: researchLineInstance])
+				return false
+			}
+		}
+		return true
+	}
 
-    def createMessage(params){
-        flash.message = message(code: 'default.deleted.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), params.id])
-        redirect(params)
-    }
+	def editPublications(researchLineInstance){
+		for(p in researchLineInstance?.publications)
+		{
+			p.researchLine = null;
+			if(!p.save(flush:true))
+			{
+				render(view: "edit", model: [researchLineInstance: researchLineInstance])
+				return false
+			}
+		}
+		true
+	}
 
-    def saveResearchLine(view, researchLineInstance, action)
-    {
-        if (!researchLineInstance.save(flush: true)) {
-            render(view: view, model: [researchLineInstance: researchLineInstance])
-        }
-        else {
-            flash.message = message(code: 'default.' + action + '.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), researchLineInstance.id])
-            redirect(action: "show", id: researchLineInstance.id)
-        }
-    }
+	def createMessage(params){
+		flash.message = message(code: 'default.deleted.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), params.id])
+		redirect(params)
+	}
 
-    def tryFindInstance(params){
-        def researchLineInstance = getInstance(params)
+	def saveResearchLine(def view, def researchLineInstance, def action,def oldMembers)
+	{
+		if (!researchLineInstance.save(flush: true)) {
+			render(view: view, model: [researchLineInstance: researchLineInstance])
+		}
+		else {
+			sendNotifications(researchLineInstance,oldMembers)
+			flash.message = message(code: 'default.' + action + '.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), researchLineInstance.id])
+			redirect(action: "show", id: researchLineInstance.id)
+		}
+	}
 
-        if (researchLineInstance)
-            [researchLineInstance: getInstance(params)]
-    }
+	def tryFindInstance(params){
+		def researchLineInstance = getInstance(params)
 
-    def getInstance(params){
-        def researchLineInstance = ResearchLine.get(params.id)
-        if (!researchLineInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), params.id])
-            redirect(action: "list")
-            return
-        }
+		if (researchLineInstance)
+			[researchLineInstance: getInstance(params)]
+	}
 
-        researchLineInstance
-    }
+	def getInstance(params){
+		def researchLineInstance = ResearchLine.get(params.id)
+		if (!researchLineInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'researchLine.label', default: 'ResearchLine'), params.id])
+			redirect(action: "list")
+			return
+		}
+
+		researchLineInstance
+	}
 }
