76787a3329205b723f4768ac91b5c1cbea9c16ec
baff <baff@cin.ufpe.br>
2013-04-22 17:45:18 -0300

    Feature news finished

---
+++ src/java/rgms/news/TwitterConnection.java
@@ -0,0 +1,45 @@
+package rgms.news;
+
+import java.util.List;
+
+import com.itextpdf.text.pdf.PdfStructTreeController.returnType;
+
+import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+public class TwitterConnection {
+	
+	private static Paging pageConfig = new Paging(1, 10);
+	private static Twitter twitter = new TwitterFactory().getInstance();
+	
+	public boolean canConnect(String user)
+	{		
+		try
+		{
+			twitter.getUserTimeline(user);
+			return true;
+		}catch (TwitterException te)
+		{
+			return false;
+		}
+		
+	}
+	
+	public List<Status> getTimeLine(String user)
+	{
+		List<Status> statusList = null;
+		try
+		{			 
+			statusList = twitter.getUserTimeline(user,pageConfig);			
+		}catch (TwitterException te)
+		{
+			te.printStackTrace();
+            System.out.println("Failed to get timeline: " + te.getMessage());
+		}
+		return statusList;
+	}
+
+}
