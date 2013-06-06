c7563b139d4ecff07ea430c42da37cbd29b55e53
netuh <waldemar.neto@gmail.com>
2013-06-06 19:16:39 -0300

    untracked Items

---
+++ grails-app/controllers/rgms/tool/TwitterTool.groovy
@@ -0,0 +1,37 @@
+package rgms.tool
+
+import twitter4j.Status
+import twitter4j.Twitter
+import twitter4j.TwitterFactory
+import twitter4j.auth.AccessToken
+
+public class TwitterTool{
+
+    final static String consumerKey = "ZHgJv54dR7ZkYuKLvBeFJA";
+    final static String consumerSecret = "9Qj0OVOSEbkVOdCl4X1XUnoHSA46lQuAF3zCd7Gdk";
+
+    static twittersHistory = []
+
+    public static void sendGTwitter(String articleName, String accessToken, String accessSecret, String text){
+        TwitterFactory factory = new TwitterFactory()
+        Twitter twitter = factory.getInstance()
+        AccessToken accessTokenO = new AccessToken(accessToken, accessSecret)
+
+        twitter.setOAuthConsumer(consumerKey, consumerSecret)
+        twitter.setOAuthAccessToken(accessTokenO)
+		def today = new Date ()
+        Status status = twitter.updateStatus(today+" - "+text)
+        twittersHistory.add([articleName : articleName, status : status.getId()])
+    }
+
+    static public boolean consult(String articleName) {
+        def twitterFound = twittersHistory.find { articleNameTwitter ->
+            articleNameTwitter.articleName == articleName
+        }
+        if (twitterFound == null){
+            return false
+        }
+        twitterFound?.status != 0
+    }
+
+}
