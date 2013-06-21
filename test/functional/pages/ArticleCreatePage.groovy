f6881c7655eb03ea96c65de9288c0bd851704f65
Paulo Borba <phmb@cin.ufpe.br>
2013-06-21 09:31:25 -0300

    Ajuste em ArticleCreatePage para funcionamento correto do teste do cenário new article web. Mudanças cosméticas nos outros arquivos.

--- test/functional/pages/ArticleCreatePage.groovy
+++ test/functional/pages/ArticleCreatePage.groovy
@@ -1,34 +1,32 @@
 package pages
 
 import geb.Page
-import pages.ArticleShowPage
-import pages.ArticlesPage
 
 class ArticleCreatePage extends Page {
-	static url = "periodico/create"
+    static url = "periodico/create"
 
-	static at = {
-		title ==~ /Criar Periodico/
-		journal != null
-	}
+    static at = {
+        title ==~ /Criar Periodico/
+        journal != null
+    }
 
-	static content = {
-		journal {
-			$("input", id: "journal")
-		}
-	}
+    static content = {
+        journal {
+            $("input", id: "journal")
+        }
+    }
 
-	def fillArticleDetails() {
-		$("form").title 	= "A theory of Software Product Line Refinement"
-		$("form").journal 	= "Theoretical Computer Science"
-		$("form").file 		= "TCS.pdf"
-		$("form").volume 	= 455
-		$("form").number 	= 1
-		$("form").pages 	= "2-30"
-		// Could parametrize, obtaining data from class TestDataAndOperations
-	}
+    def fillArticleDetails() {
+        $("form").title = "A theory of Software Product Line Refinement"
+        $("form").journal = "Theoretical Computer Science"
+        $("form").file = "./test/functional/steps/TCS.pdf"
+        $("form").volume = 455
+        $("form").number = 1
+        $("form").pages = "2-30"
+        // Could parametrize, obtaining data from class TestDataAndOperations
+    }
 
-	def selectCreateArticle(){
-		$("input", name: "create").click()
-	}
+    def selectCreateArticle() {
+        $("input", name: "create").click()
+    }
 }
