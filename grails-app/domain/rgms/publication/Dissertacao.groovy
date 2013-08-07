c0fee650c9b6daed8d3b9426b8ba9fb304239787
joaovlm <joaovlm123@gmail.com>
2013-08-06 23:32:35 -0300

    Remoção de clone na geração de bibtex a partir de uma dissertação. O ano da dissertação era exibido 2 vezes no arquivo bibtex gerado.

--- grails-app/domain/rgms/publication/Dissertacao.groovy
+++ grails-app/domain/rgms/publication/Dissertacao.groovy
@@ -5,7 +5,7 @@ class Dissertacao extends TeseOrDissertacao {
 
 //#if($Bibtex)
     String generateBib() {
-        return "@masterthesis{" + super.members.get(0) + super.publicationDate.getAt(Calendar.YEAR)
+        return "@masterthesis{" + super.members.get(0)
         +", author=\"" + BibtexAux.organizeAuthors(super.members) + "\",\n title=\"" + super.title + "\",\n school=\""
         +this.school + "\",\n year=\"" + super.publicationDate.getAt(Calendar.YEAR) + "\",\n address=\""
         +this.address + "\",\n month=\"" + super.publicationDate.getAt(Calendar.MONTH) + "\"}"
