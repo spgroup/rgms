5125e57e7391a6afa4a2d3fc16c825de50926a9c
Paulo Henrique da Cruz Pereira <phcp@cin.ufpe.br>
2013-08-13 16:41:25 -0300

    Remove overrides em Ferramenta
    Esses overrides já existiam em Publication, por isso não são
    necessários.

--- grails-app/domain/rgms/publication/Ferramenta.groovy
+++ grails-app/domain/rgms/publication/Ferramenta.groovy
@@ -16,25 +16,4 @@ class Ferramenta extends Publication {
         return "@misc{" + super.members.get(0) + super.publicationDate.getAt(Calendar.YEAR) + ",author=\"" + BibtexAux.organizeAuthors(super.members) + "\",\n title=\"" + super.title + "\",\n website=\"" + this.website + "\",\n year=\"" + super.publicationDate.getAt(Calendar.YEAR) + "\",\n description=\"" + this.description + "\",\n month=\"" + super.publicationDate.getAt(Calendar.MONTH) + "\"}"
     }
 //#end
-
-    @Override
-    String toString(){
-        return website
-    }
-
-    @Override
-    boolean equals(Object obj){
-        Ferramenta f = (Ferramenta) obj
-        return website.equals(f.website)
-    }
-
-    @Override
-    int hashCode(){
-        if(website == null){
-            return "".hashCode()
-        }
-        else{
-            return website.hashCode()
-        }
-    }
 }
