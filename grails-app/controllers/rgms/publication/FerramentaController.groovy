704fce874da7fbcb0ae323ad83a5a64f812e3e5b
Paulo Henrique da Cruz Pereira <phcp@cin.ufpe.br>
2013-08-13 00:16:58 -0300

    Corrige problemas encontrados pelo CodeNarc na feature Ferramenta
    - ImportFromSamePackage em FerramentaController;
    - GrailsDomainHasEquals, GrailsDomainHasToString e DeadCode em
    Ferramenta;

--- grails-app/controllers/rgms/publication/FerramentaController.groovy
+++ grails-app/controllers/rgms/publication/FerramentaController.groovy
@@ -4,7 +4,6 @@ import org.springframework.dao.DataIntegrityViolationException
 //#if($upXMLFerramenta)
 import rgms.XMLService
 //#end
-import rgms.publication.Ferramenta;
 
 class FerramentaController {
 
--- grails-app/domain/rgms/publication/Ferramenta.groovy
+++ grails-app/domain/rgms/publication/Ferramenta.groovy
@@ -13,10 +13,28 @@ class Ferramenta extends Publication {
 
 //#if($Bibtex)
     String generateBib() {
-        return "@misc{"
-        + "author=\"" + BibtexAux.organizeAuthors(members) + "\",\n title=\"" + title + "\",\n website=\""
-        +this.website + "\",\n year=\"" + publicationDate.getAt(Calendar.YEAR) + "\",\n description=\""
-        +this.description + "\",\n month=\"" + publicationDate.getAt(Calendar.MONTH) + "\"}"
+        return "@misc{" + super.members.get(0) + super.publicationDate.getAt(Calendar.YEAR) + ",author=\"" + BibtexAux.organizeAuthors(super.members) + "\",\n title=\"" + super.title + "\",\n website=\"" + this.website + "\",\n year=\"" + super.publicationDate.getAt(Calendar.YEAR) + "\",\n description=\"" + this.description + "\",\n month=\"" + super.publicationDate.getAt(Calendar.MONTH) + "\"}"
     }
 //#end
+
+    @Override
+    String toString(){
+        return website
+    }
+
+    @Override
+    boolean equals(Object obj){
+        Ferramenta f = (Ferramenta) obj
+        return website.equals(f.website)
+    }
+
+    @Override
+    int hashCode(){
+        if(website == null){
+            return "".hashCode()
+        }
+        else{
+            return website.hashCode()
+        }
+    }
 }
