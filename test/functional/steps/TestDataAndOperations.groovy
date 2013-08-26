accc94a66540f790519bceb13b7fef873006abf4
Diogo Filipe Dornelas Falcao <diogofilipedf@hotmail.com>
2013-08-26 20:13:14 -0300

    Extraido metodos de testes de conferencia em ConferenciaTestDataAndOperations

--- test/functional/steps/TestDataAndOperations.groovy
+++ test/functional/steps/TestDataAndOperations.groovy
@@ -49,18 +49,6 @@ class TestDataAndOperations {
                     publisher: "Penguim", chapter: 3]
     ]
 
-    static conferencias = [
-            [title: "I International Conference on Software Engineering",
-                    publicationDate: (new Date("12 October 2012")),
-                    booktitle: "Software Engineering", pages: "20-120"],
-            [title: "IV Conference on Software Product Lines",
-                    publicationDate: (new Date("14 October 2012")),
-                    booktitle: "Practices and Patterns", pages: "150-200"],
-            [title: "V Conference on Software Product Lines",
-                    publicationDate: (new Date("16 October 2012")),
-                    booktitle: "Practices and Patterns", pages: "50-100"]
-    ]
-
     static records = [
             [status_H: "MSc Student", start: (new Date()), end: null],
             [status_H: "Graduate Student", start: (new Date()), end: null]
@@ -166,12 +154,6 @@ class TestDataAndOperations {
     static public def findBookChapterByTitle(String title) {
         bookChapters.find { bookChapter ->
             bookChapter.title == title
-        }
-    }
-
-    static public def findConferenciaByTitle(String title) {
-        conferencias.find { conferencia ->
-            conferencia.title == title
         }
     }
 
@@ -324,20 +306,6 @@ class TestDataAndOperations {
         cont.response.reset()
     }
 
-    static public boolean conferenciaCompatibleTo(conferencia, title) {
-        def testConferencia = findConferenciaByTitle(title)
-        def compatible = false
-        if (testConferencia == null && conferencia == null) {
-            compatible = true
-        } else if (testConferencia != null && conferencia != null) {
-            compatible = true
-            testConferencia.each { key, data ->
-                compatible = compatible && (conferencia."$key" == data)
-            }
-        }
-        return compatible
-    }
-
     static public void createArticle(String title, filename) {
         def cont = new PeriodicoController()
         def date = new Date()
@@ -407,16 +375,6 @@ class TestDataAndOperations {
         researchGroupController.request.setContent(new byte[1000]) // Could also vary the request content.
         researchGroupController.delete()
         researchGroupController.response.reset()
-    }
-
-
-    static public void createConferencia(String title, String filename) {
-        def cont = new ConferenciaController()
-        cont.params << TestDataAndOperations.findConferenciaByTitle(title) << [file: filename]
-        cont.request.setContent(new byte[1000])
-        cont.create()
-        cont.save()
-        cont.response.reset()
     }
 
     static public void createTechnicalReport(String title, filename) {
@@ -636,12 +594,6 @@ class TestDataAndOperations {
 
         def updatedferramenta = Ferramenta.findByTitle(newtitle)
         return updatedferramenta
-    }
-
-    static public void removeConferencia(String title) {
-        def cont = new ConferenciaController()
-        def date = new Date()
-        Conferencia.findByTitle(title).delete(flush: true)
     }
 
 //#if ( $visit )
