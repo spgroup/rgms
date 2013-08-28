bf8148ca5b13dc2dc8961f63bfbf19f4e0c3612d
Alberto Rodrigues <arcj@cin.ufpe.br>
2013-08-27 23:36:08 -0300

    Criação do TestAndOperationsResearchGroup. Existem outros métodos de que utilizam ResearchGroupController mas pertecem a outras features.

--- test/functional/steps/TestDataAndOperations.groovy
+++ test/functional/steps/TestDataAndOperations.groovy
@@ -392,39 +392,6 @@ class TestDataAndOperations {
         cont.response.reset()
     }
 
-    static public void createResearchGroup(String name, description) {
-        def researchGroupController = new ResearchGroupController()
-        researchGroupController.params << [name: name] << [description: description]
-        researchGroupController.request.setContent(new byte[1000]) // Could also vary the request content.
-        researchGroupController.create()
-        researchGroupController.save()
-        researchGroupController.response.reset()
-    }
-
-    static public void editResearchGroup(def researchGroup, String newName, String newDescription) {
-        def researchGroupController = new ResearchGroupController()
-        researchGroupController.params << [name: newName] << [description: newDescription] << [id: researchGroup.getId()]
-        researchGroupController.request.setContent(new byte[1000]) // Could also vary the request content.
-        researchGroupController.edit()
-        researchGroupController.save()
-        researchGroupController.response.reset()
-    }
-
-    //#if($researchGroupHierarchy)
-    static public void editResearchGroupChildOf(ResearchGroup researchGroup, ResearchGroup researchGroupParent) {
-        def researchGroupController = new ResearchGroupController()
-        researchGroupController.params << [name: researchGroup.name]
-        researchGroupController.params << [description: researchGroup.description]
-        researchGroupController.params << [id: researchGroup.id]
-        researchGroupController.params << [childOf: researchGroupParent]
-        researchGroupController.request.setContent(new byte[1000]) // Could also vary the request content.
-
-        try {
-            researchGroupController.update()
-        } catch (Exception e) {}
-    }
-    //#end
-
     static public void createBookChapter(String title, filename) {
         def cont = new BookChapterController()
         def date = new Date()
@@ -435,13 +402,6 @@ class TestDataAndOperations {
         cont.response.reset()
     }
 
-    static public void deleteResearchGroup(def researchGroup) {
-        def researchGroupController = new ResearchGroupController()
-        researchGroupController.params << [id: researchGroup.getId()]
-        researchGroupController.request.setContent(new byte[1000]) // Could also vary the request content.
-        researchGroupController.delete()
-        researchGroupController.response.reset()
-    }
 
 
     static public void createConferencia(String title, String filename) {
