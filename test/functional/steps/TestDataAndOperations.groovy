a0ab0602c578df94d54149cff7ad0645d52725d0
joaovlm <joaovlm123@gmail.com>
2013-08-29 12:00:38 -0300

    [Atividade 7] Modularização dos métodos relativos à feature Bibtex import que estavam contidos no arquivo TestDataAndOperations em um novo arquivo

--- test/functional/steps/TestDataAndOperations.groovy
+++ test/functional/steps/TestDataAndOperations.groovy
@@ -91,10 +91,6 @@ class TestDataAndOperations {
         BibtexFileController bibtexFileController = new BibtexFileController()
         BibtexFile bibtexFile = bibtexFileController.transform(new File(path))
     }
-    static public def openBibTexFile(String path){
-        BibtexFileController bibtexFileController = new BibtexFileController()
-        BibtexFile bibtexFile = bibtexFileController.transform(new File(path))
-    }
 
     static public def findFerramentaByTitle(String title) {
         ferramentas.find { ferramenta ->
