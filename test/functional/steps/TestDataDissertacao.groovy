659077529f5fe7f308e7e977c9ecc2ebd3bd0edb
raonybenjamim <raonybenjamim@gmail.com>
2013-08-29 11:57:50 -0300

    [Atividade7] Separação de funções referentes à feature de dissertação em arquivo separado de TesteDataAndOperations visando a melhoria da modularização do código

---
+++ test/functional/steps/TestDataDissertacao.groovy
@@ -0,0 +1,72 @@
+package steps
+
+import rgms.member.Member
+import rgms.publication.*
+
+class TestDataDissertacao
+{
+
+    static public void createDissertacao(String title, filename, school) {
+        def cont = new DissertacaoController()
+        def date = new Date()
+        cont.params << [title: title, publicationDate: new Date(2013, 03, 02),
+                school: school, address: "Boa Viagem", file: filename]
+        cont.request.setContent(new byte[1000]) // Could also vary the request content.
+        cont.create()
+        cont.save()
+        cont.response.reset()
+    }
+
+
+    static public void createDissertacaoWithotSchool(String title, filename) {
+        def cont = new DissertacaoController()
+        def date = new Date()
+        cont.params << [title: title, publicationDate: new Date(2013, 03, 02),
+                address: "Boa Viagem", file: filename]
+        cont.request.setContent(new byte[1000]) // Could also vary the request content.
+        cont.create()
+        cont.save()
+        cont.response.reset()
+    }
+    static public void createDissertacaoWithoutAddress(String title, filename) {
+        def cont = new DissertacaoController()
+        def date = new Date()
+        cont.params << [title: title, publicationDate: new Date(2013, 03, 02),school: "UFPE", file: filename]
+        cont.request.setContent(new byte[1000]) // Could also vary the request content.
+        cont.create()
+        cont.save()
+        cont.response.reset()
+    }
+
+    static public Dissertacao editDissertatacao(oldtitle, newtitle) {
+        def article = Dissertacao.findByTitle(oldtitle)
+        article.setTitle(newtitle)
+        def cont = new DissertacaoController()
+        cont.params << article.properties
+        cont.update()
+
+        def updatedarticle = Dissertacao.findByTitle(newtitle)
+        return updatedarticle
+    }
+
+    static public void uploadDissertacao(filename) {
+        def cont = new DissertacaoController()
+        def xml = new File(filename);
+        def records = new XmlParser()
+        cont.saveDissertations(records.parse(xml));
+        cont.response.reset()
+    }
+
+    static public void removeDissertacao(String title) {
+        def testDissertation = Dissertacao.findByTitle(title)
+        def cont = new DissertacaoController()
+        def date = new Date()
+        cont.params << [id: testDissertation.id]
+        cont.delete()
+    }
+
+    static public boolean containsUser(members){
+        def userData = Member.findByUsername('admin').id.toString()
+        return members.contains(userData)
+    }
+}
