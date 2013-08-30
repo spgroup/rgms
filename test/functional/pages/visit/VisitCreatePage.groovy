1b376a0764da185b320ead33916203bdb6a692f6
carloscemb <cemb@cin.ufpe.br>
2013-08-30 18:38:19 -0300

    Update VisitCreatePage.groovy

--- test/functional/pages/visit/VisitCreatePage.groovy
+++ test/functional/pages/visit/VisitCreatePage.groovy
@@ -7,12 +7,12 @@ class VisitCreatePage extends Page {
     static url = "visit/create"
 
     static at = {
-        title ==~ /Criar Visita/
-        /*GetPageTitle gp = new GetPageTitle()
+        //title ==~ /Criar Visita/
+        GetPageTitle gp = new GetPageTitle()
         def currentVisit = gp.msg("default.visit.label")
         def currentTitle = gp.msg("default.create.label", [currentVisit])
 
-        title ==~ currentTitle */
+        title ==~ currentTitle 
     }
 
     static content = {
