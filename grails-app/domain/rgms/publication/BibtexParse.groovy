584408a14cb90915d17cbe07683a824570719a3c
raonybenjamim <raonybenjamim@gmail.com>
2013-09-02 15:46:53 -0300

    [Atividade 8] adição de função para o tratamento da lista de membros contida no arquivo bibtex importado

--- grails-app/domain/rgms/publication/BibtexParse.groovy
+++ grails-app/domain/rgms/publication/BibtexParse.groovy
@@ -4,6 +4,7 @@ import org.jbibtex.*
 import rgms.publication.strategyBibtexParse.StrategyParseDissertacao
 import rgms.publication.strategyBibtexParse.StrategyParseTese
 
+
 /**
  *
  * @author Diogo Vin�cius
@@ -26,11 +27,13 @@ class BibtexParse {
                     return null  //To change body of implemented methods use File | Settings | File Templates.
                 }
             }
+
             Date date = new Date()
             date.year = entry.getField(BibTeXEntry.KEY_YEAR).toUserString().toInteger()
             publicationTemp.title = entry.getField(BibTeXEntry.KEY_TITLE).toUserString()
             publicationTemp.publicationDate = date
-            publicationTemp.members = entry.getField(BibTeXEntry.KEY_AUTHOR).toUserString().toSet()
+            publicationTemp.members = entry.getField(BibTeXEntry.KEY_AUTHOR).toUserString()
+            System.println("======"+publicationTemp.members)
             if (entry.getType().equals(BibTeXEntry.TYPE_ARTICLE)) {
 
             } else if (entry.getType().equals(BibTeXEntry.TYPE_BOOK)) {
@@ -76,6 +79,11 @@ class BibtexParse {
 
     }
 
+    private static extractMembersFromBibtexEntry (String entry){
+        entry.toLowerCase()
+        String[] stringArray;
+        stringArray = entry.split('and')
+    }
     private static BibTeXDatabase parseBibTeX(File file) throws IOException, ParseException {
         Reader reader = new FileReader(file);
 
