d28a78e1156e0f7f137520e9d5fa2467b7d523f8
raonybenjamim <raonybenjamim@gmail.com>
2013-09-02 21:27:24 -0300

    [Atividade 8] Implementação do step para submissão de arquivos bibtex e melhoria no path de abertura dos arquivos bibtex

--- test/cucumber/steps/BibtexImportSteps.groovy
+++ test/cucumber/steps/BibtexImportSteps.groovy
@@ -21,27 +21,32 @@ Given(~'^I am on Import Bibtex File Menu$') {->
 }
 
 When(~'^I click "([^"]*)"$') { String arg1 ->
+    System.println(arg1)
     at BibtexImportPage
     page.selectButton(arg1)
 }
 
 When(~'^I select a bibtex file and I click "([^"]*)"$') { String arg1 ->
+    System.println(arg1)
+    BibtexFile bibtexFile = TestDataBibTexFile.openBibTexFile("sample.bibtex")
+    page.selectButton(arg1)
+
 }
 
 Then(~'^all corresponding publications are created$') {->
-    BibtexFile bibtexFile = TestDataBibTexFile.openBibTexFile("test//cucumber//steps//sample.bibtex")
+    BibtexFile bibtexFile = TestDataBibTexFile.openBibTexFile("sample.bibtex")
     assert bibtexFile.getPublications().size() == 3
 }
 
 Then(~'^all of then are stored$') {->
 }
 
-When(~'^I select a bibtex file unformatted and I click "([^"]*)"$') { String arg1 ->
+When(~'^I select a bibtex file unformatted and I click "([^"]*)"$') { String arg1 -> 6
 }
 
 //@Test(expected=RuntimeException.class)
 Then(~'^the system output the message error "([^"]*)"$') { String arg1 ->
-    BibtexFile bibtexFile = TestDataBibTexFile.openBibTexFile("test//cucumber//steps//sample.bibtex")
+    BibtexFile bibtexFile = TestDataBibTexFile.openBibTexFile("sample.bibtex")
 }
 
 Then(~'^none publication is stored$') {->
@@ -52,13 +57,13 @@ When(~'^I select a bibtex file with one Dissertation and two Thesis and I click 
 }
 
 Then(~'^is created one Dissertation publication$') {->
-    BibtexFile bibtexFile = TestDataBibTexFile.openBibTexFile("test//cucumber//steps//sample.bibtex")
+    BibtexFile bibtexFile = TestDataBibTexFile.openBibTexFile("sample.bibtex")
     assert bibtexFile.getPublications(Conferencia.class).isEmpty()
     assert bibtexFile.getPublications(Dissertacao.class).size() == 1
 }
 
 Then(~'^is created two Thesis publications$') {->
-    BibtexFile bibtexFile = TestDataBibTexFile.openBibTexFile("test//cucumber//steps//sample.bibtex")
+    BibtexFile bibtexFile = TestDataBibTexFile.openBibTexFile("sample.bibtex")
     assert bibtexFile.getPublications(TechnicalReport.class).isEmpty()
     assert bibtexFile.getPublications(Tese.class).size() == 2
 }
--- test/functional/steps/TestDataBibTexFile.groovy
+++ test/functional/steps/TestDataBibTexFile.groovy
@@ -5,7 +5,8 @@ import rgms.publication.BibtexFileController
 
 class TestDataBibTexFile
 {
-    static public def openBibTexFile(String path){
+    static public def openBibTexFile(String filename){
+        def path = new File(".").getCanonicalPath() + File.separator + "test" + File.separator + "cucumber" + File.separator + "steps" + File.separator + filename
         BibtexFileController bibtexFileController = new BibtexFileController()
         BibtexFile bibtexFile = bibtexFileController.transform(new File(path))
     }
