77284210b680705023954faa3c7f2857b48eca79
Diogo Filipe Dornelas Falcao <diogofilipedf@hotmail.com>
2013-09-03 21:40:10 -0300

    Adicionado teste para a feature "Gerar todos os bibtex"

---
+++ test/functional/pages/BibtexGenerateFilePage.groovy
@@ -0,0 +1,38 @@
+package pages
+
+import geb.Page
+import rgms.publication.BookChapter
+
+class BibtexGenerateFilePage extends FormPage {
+    static url = "bookChapter/create"
+
+    static at = {
+        title ==~ /Criar BookChapter/
+        publisher != null
+    }
+
+    static content = {
+        publisher {
+            $("input", id: "publisher")
+        }
+        flashmessage {
+            $("div", class: "message")
+        }
+    }
+
+    def fillBookChapterDetails(title, filename){
+        fillTitle(title)
+        $("form").publisher = "Person"
+        $("form").chapter = 1
+        $("form").file = new File(".").getCanonicalPath() + File.separator + "test" + File.separator + "functional" + File.separator + "steps" + File.separator + filename
+    }
+
+    def clickSaveBookChapter(){
+        $("form").create().click()
+    }
+
+    def fillTitle(title) {
+        $("form").title = title
+        clickSaveBookChapter()
+    }
+}
