c0d8691515c4e0f757246ddf48ba76a1ec6398ad
pedrootorres <phtg@cin.ufpe.br>
2014-01-30 12:05:59 -0300

    Criando classe CommonPages

--- test/functional/pages/Util/CommonPages.groovy
+++ test/functional/pages/Util/CommonPages.groovy
@@ -1,7 +1,15 @@
 package pages.Util
 
+import geb.Page
+
+
 /**
  * Created by rafael on 1/29/14.
  */
 class CommonPages {
+    def static selectView(page, title) {
+        page.getRow()
+        def showLink = page.getRow().find('td').find([text:title])
+        showLink.click()
+    }
 }
