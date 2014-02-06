a86a4d19fef71ec4ca6af3dd88c6c84614d80007
JGFD <jgfd@cin.ufpe.br>
2014-02-06 17:04:59 -0300

    Parameterize Method com os métodos upload e newupload de controlador de Publication

--- grails-app/controllers/rgms/publication/PublicationController.groovy
+++ grails-app/controllers/rgms/publication/PublicationController.groovy
@@ -64,40 +64,28 @@ class PublicationController {
 
     def upload(Publication publicationInstance) {
 
-        def originalName = publicationInstance.file
-        def filePath = "web-app/uploads/${originalName}"
-        publicationInstance.file = filePath
+        auxUpload(publicationInstance, flash, request, message(code: 'file.already.exist.message', default: 'File already exists. Please try to use a different file name.'))
 
-        def f = new File(filePath)
-
-        if (f.exists()) {
-            flash.message = message(code: 'file.already.exist.message', default: 'File already exists. Please try to use a different file name.')
-            return false
-        }
-
-        InputStream inputStream = request.getInputStream()
-        OutputStream outputStream = new FileOutputStream(f)
-        byte[] buffer = new byte[1024 * 10] //buffer de 10MB
-        int length
-
-        while ((length = inputStream.read(buffer)) > 0) {
-            outputStream.write(buffer, 0, length)
-        }
-        outputStream.close()
-        inputStream.close()
-
-        return true
     }
+
+
 
     def static newUpload(Publication publicationInstance, flash, request) {
 
+        auxUpload(publicationInstance, flash, request, 'File already exists. Please try to use a different file name.')
+
+    }
+
+    def static auxUpload(Publication publicationInstance, flash, request, message) {
+
         def originalName = publicationInstance.file
         def filePath = "web-app/uploads/${originalName}"
         publicationInstance.file = filePath
 
         def f = new File(filePath)
+
         if (f.exists()) {
-            flash.message = 'File already exists. Please try to use a different file name.'
+            flash.message = message
             return false
         }
         InputStream inputStream = request.getInputStream()
@@ -112,6 +100,7 @@ class PublicationController {
         inputStream.close()
 
         return true
+
     }
 
 	/**
