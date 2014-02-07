481572e34d81f91c95df2dec86296b6c37061ce7
Dyego <dfop@cin.ufpe.br>
2014-02-07 00:43:56 -0200

    corrigindo return de checkExistingNewsByDescription

--- test/functional/steps/NewsTestDataAndOperations.groovy
+++ test/functional/steps/NewsTestDataAndOperations.groovy
@@ -33,11 +33,7 @@ class NewsTestDataAndOperations {
 
     static public boolean checkExistingNewsByDescription(String description) {
         def news = News.findByDescription(description)
-        if ((news?.date != null) && (news?.id != 0) && (news?.researchGroup != null)) {
-            return true
-        } else {
-            return false
-        }
+        return news != null
     }
 
     static public void editNewsDescription(String description, String newDescription, Date date, ResearchGroup researchGroup) {
