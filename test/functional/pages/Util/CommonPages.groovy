097c2c7b36ada6f66c2fd7ec7154ab11f5e46dc7
pedrootorres <phtg@cin.ufpe.br>
2014-02-10 20:15:48 -0300

    CommonPages herdando de Page

--- test/functional/pages/Util/CommonPages.groovy
+++ test/functional/pages/Util/CommonPages.groovy
@@ -2,11 +2,10 @@ package pages.Util
 
 import geb.Page
 
-
 /**
  * Created by rafael on 1/29/14.
  */
-class CommonPages {
+class CommonPages extends Page {
     def static selectView(page, title) {
         page.getRow()
         def showLink = page.getRow().find('td').find([text:title])
