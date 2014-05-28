a0dbd7ac16db31feaa692943fa794a256583d1bb
walber-nunes <walber026@gmail.com>
2014-05-28 01:56:07 -0300

    remoção de imports não utilizados ,implementação de alguns testes, criação do atributo 'authors' do tipo 'Set<Member>' na classe 'Conferencia' e criação do método 'containsAuthor(String memberName, String title)' na classe 'ConferenciaTestDataAndOperations'

--- test/functional/steps/TestDataAndOperationsPublication.groovy
+++ test/functional/steps/TestDataAndOperationsPublication.groovy
@@ -1,11 +1,10 @@
 package steps
 
 import rgms.authentication.User
-import rgms.member.*
 
 class TestDataAndOperationsPublication {
 
-    static public boolean containsUser(members){
+    static public boolean containsUser(members) {
         def userData = User.findByUsername('admin')?.author?.id.toString()
         return members.contains(userData)
     }
