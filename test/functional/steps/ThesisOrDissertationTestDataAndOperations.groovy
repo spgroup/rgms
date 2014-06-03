b2dc7962f1431fd40ff76b5e6c629cf443ce9646
elliscarvalho <ellisvcarvalho@gmail.com>
2014-06-02 21:51:23 -0300

    Correção e modificação nos testes a partir dos comentários encontrados no Git.

--- test/functional/steps/ThesisOrDissertationTestDataAndOperations.groovy
+++ test/functional/steps/ThesisOrDissertationTestDataAndOperations.groovy
@@ -3,10 +3,9 @@ package steps
 import rgms.publication.Dissertacao
 import rgms.publication.Tese
 import rgms.publication.TeseController
-
 class ThesisOrDissertationTestDataAndOperations {
 
-    static protected void createThesisOrDissertation(String title, filename, school, cont) {
+    static void createThesisOrDissertation(String title, filename, school, cont) {
         cont.params << [title: title, publicationDate: new Date(2013, 03, 02),
                 school: school, address: "Boa Viagem", file: filename]
         cont.request.setContent(new byte[1000]) // Could also vary the request content.
