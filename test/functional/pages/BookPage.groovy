68800dc7d026a4f9e1e79a393ab77e741e08def8
Tomer Simis <tdl.simis@gmail.com>
2015-01-20 00:09:09 -0300

    Criação de métodos para ordenação de livros

--- test/functional/pages/BookPage.groovy
+++ test/functional/pages/BookPage.groovy
@@ -33,4 +33,33 @@ class BookPage extends Page {
     def checkDownloadLink(String filename){
         return $('a', text:filename) != null
     }
+    
+    def selectOrderBy(String orderType){
+        $('a', text: orderType).click()
+    }
+
+    def checkOrderedBy(String orderType){
+        def header = $('table').find('thead').find('tr').find('td')
+        def rows = $('table').find('tbody').find('tr')
+        def dates = []
+
+        // Recupera o indice da coluna relativo ao tipo de ordenacao
+        def index = 1
+        def found = false
+        for(td in header){
+            if(!found){
+                if(td.text() == orderType){
+                    found = true;
+                }
+                index++;
+            }
+        }
+
+        // Adiciona em uma lista para verificar se essa lista é identica a lista ordenada
+        for(row in rows){
+            dates.add(row.find('td:nth-child(' + index + ')').text())
+        }
+
+        return dates == dates.sort()
+    }
 }
