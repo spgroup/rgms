56325518828b5e04dbbc3f8a0511ae1c39a76913
lrexxx <lre@cin.ufpe.br>
2015-01-20 21:46:39 -0300

    Adicionando metodos auxiliares para teste

--- test/functional/steps/ThesisOrDissertationTestDataAndOperations.groovy
+++ test/functional/steps/ThesisOrDissertationTestDataAndOperations.groovy
@@ -6,6 +6,21 @@ import rgms.publication.TeseController
 
 class ThesisOrDissertationTestDataAndOperations {
 
+    static teses = [
+            [title: "TCS-01", publicationDate: "01/02/2014", file: "TCS-01.pdf",
+             researchLine: "Computation", school: "UFPE"
+            ],
+            [title: "TCS-02", publicationDate: "01/06/2014", file: "TCS-02.pdf",
+             researchLine: "Statistics", school: "UFPE"
+            ],
+            [title: "TCS-03", publicationDate: "01/03/2014", file: "TCS-03.pdf",
+             researchLine: "Algebra", school: "UFPE"
+            ],
+            [title: "TCS-04", publicationDate: "01/04/2014", file: "TCS-04.pdf",
+             researchLine: "Mathmatics", school: "UFPE"
+            ],
+    ]
+
     static protected void createThesisOrDissertation(String title, filename, school, cont) {
         cont.params << [title: title, publicationDate: new Date(2013, 03, 02),
                 school: school, address: "Boa Viagem", file: filename]
@@ -26,4 +41,10 @@ class ThesisOrDissertationTestDataAndOperations {
         cont.delete()
 
     }
+    
+    static public def findByTitle(String title){
+                teses.find{ tese ->
+                        tese.title == title
+                    }
+            }
 }
